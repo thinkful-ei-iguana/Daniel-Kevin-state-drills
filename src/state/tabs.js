b194d25f7078c35a030e2300790221d8ab889532
Kevin Moore <moore.kevin.p@gmail.com>
2019-11-13 14:45:05 -0500

    event handlers and conditional work

--- src/state/tabs.js
+++ src/state/tabs.js
@@ -1,26 +1,37 @@
 import React from "react";
 
-class Tabs extends React.Component {
-    static defaultProps = {tabs: []};
-    render() {
-      const buttons = this.props.tabs.map((tab, index) =>(
-          <button key={index}>
-              {tab.name}
+export default class Tabs extends React.Component {
+    static defaultProps = {
+        tabs: []
+    };
+    state = {
+        currentTabIndex: 0
+    }
+    handleButtonClick = (index) => {
+        this.setState({currentTabIndex: index})
+        console.log('button clicked!', {index})
+    }
+    renderButtons() {
+        return this.props.tabs.map((tab, index) => (
+          <button key={index} onClick={() => this.handleButtonClick(index)}>
+            {tab.name}
           </button>
-      ))
-      const currentTab = this.props.tabs[0]
-      console.log(currentTab)
-    return (    
-        <div>
-            {buttons}
-            {this.props.tabs.length && (
-            <div className='content'>
-                 {currentTab.content}
-            </div>
-             )}
-        </div>
-    )
-  }
+        ))
+      }
+      renderContent() {
+        const currentTab = this.props.tabs[this.state.currentTabIndex]
+        return (
+          <div className='content'>
+            {currentTab.content}
+          </div>
+        )
+      }
+      render() {
+        return (
+          <div>
+            {this.renderButtons()}
+            {this.props.tabs.length && this.renderContent()}
+          </div>
+        )
+        }
 }
-
-export default Tabs;
